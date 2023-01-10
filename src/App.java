2790cfd42f22e244d0fa8d8f109eadf57b8b9d5d
Lucas Palharini <lucas230991@hotmail.com>
2023-01-10 14:50:43 -0300

    :sparkles: feat: pyramid of numbers completed

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        System.out.println("Vamos criar uma pirâmide numérica\nEscolha o tamanho dela:");
+        boolean error = false;
+        
+        try(Scanner input = new Scanner(System.in)) {
+
+            final int size = input.nextInt();
+
+            System.out.println("\n");
+
+            for(int i = 1; i <= size; i++) {
+                String letter = Integer.toString(i);
+                String line = letter.repeat(i);
+
+                System.out.println(line.substring(0, i));
+
+            }
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+        } finally {
+            System.out.println("\nFeito =D\n");
+        }
     }
 }
