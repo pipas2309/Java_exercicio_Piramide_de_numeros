d938e2ad8747c9cb5334d5fcd1dc2df9d046c318
Lucas Palharini <lucas230991@hotmail.com>
2023-01-10 18:24:43 -0300

    :sparkles: feat: new pyramid building mode

--- src/App.java
+++ src/App.java
@@ -3,7 +3,16 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
         
-        System.out.println("Vamos criar uma pirâmide numérica\nEscolha o tamanho dela:");
+        System.out.println("\nQuer construir uma SUPER pirâmide?\nDigite Sim ou apenas pressione enter");
+
+        Scanner scanner = new Scanner(System.in);
+        String readString = scanner.nextLine();
+        boolean huge = false;
+
+        if(readString.equalsIgnoreCase("sim")) huge = true;
+
+
+        System.out.println("\n\nVamos criar uma pirâmide numérica\nEscolha o tamanho dela:");
         boolean error = false;
         
         try(Scanner input = new Scanner(System.in)) {
@@ -12,11 +21,20 @@ public class App {
 
             System.out.println("\n");
 
-            for(int i = 1; i <= size; i++) {
+            if(!huge) {
+                for(int i = 1; i <= size; i++) {
 
-                String line = Integer.toString(i).repeat(i);
+                    String line = Integer.toString(i).repeat(i);
+    
+                    System.out.println(line.substring(0, i));
+                }
+            } else {
+                for(int i = 1; i <= size; i++) {
 
-                System.out.println(line.substring(0, i));
+                    String line = " ".repeat(size - i) + Integer.toString(i).repeat(i) + Integer.toString(i).repeat(i);
+    
+                    System.out.println(line.substring(0, (size - i) + i*2 - 1));
+                }
             }
             
         } catch (java.util.InputMismatchException e) {
@@ -27,8 +45,11 @@ public class App {
             System.out.println("-------------\n");
 
         } finally {
-            if(!error) {
+            scanner.close();
+            if(!error && !huge) {
                 System.out.println("\nVocê já está pronto para o /\\ TelexFree /\\\n");
+            } else if(!error) {
+                System.out.println("\n   - - -  I L L U M I N A T I  - - -\n");
             }
         }
     }
